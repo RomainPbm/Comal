79b00090e1b6c9405c2ccdb05da6da9a4589eca3
Perezbolde Marmion Romain T <romainperezboldemarmion@gmail.com>
2023-12-10 23:01:47 -0600

    poi

--- Comal/src/main/java/com/mycompany/comal/Catalog.java
+++ Comal/src/main/java/com/mycompany/comal/Catalog.java
@@ -12,4 +12,4 @@ public class Catalog {
     
 }
 
-public
+
--- Comal/src/main/java/com/mycompany/comal/User.java
+++ Comal/src/main/java/com/mycompany/comal/User.java
@@ -9,5 +9,45 @@ package com.mycompany.comal;
  * @author romai
  */
 public class User {
+    private int file;
+    private String Name;
+    private double credit; 
+    private double addcredit;
+
+    public User(int file, double credit, double addcredit, String Name) {
+        this.file = file;
+        this.credit = credit;
+        this.addcredit = addcredit;
+        this.Name = Name;
+    }
+
+    public int getFile() {
+        return file;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public double getAddcredit() {
+        return addcredit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "file=" + file + ", Name=" + Name + ", credit=" + credit + ", addcredit=" + addcredit + '}';
+    }
+
+    
+
+    
     
 }
